d1c284b2caa9c9c6acace2a012656ecc0c7f61e3
Chidi Okongwu <chidiokongwu@gmail.com>
2021-12-09 02:33:23 -0500

    tagDao tests implemented

--- CapstoneBlog/src/test/java/blog/dao/DaoTagImplTest.java
+++ CapstoneBlog/src/test/java/blog/dao/DaoTagImplTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = TestApplicationConfiguration.class)
 public class DaoTagImplTest {
@@ -32,22 +35,51 @@ public class DaoTagImplTest {
 
     @Test
     void testAddGetTag() {
+        Tag tag = new Tag();
+        tag.setHashTag("test");
+        Tag tag2 = new Tag();
+        tag2.setHashTag("test2");
+        tagDao.addTag(tag);
+        tagDao.addTag(tag2);
+
+        List<Tag> actual = tagDao.getAllTags();
+        List<Tag> expected = new ArrayList<>();
+        expected.add(tag);
+        expected.add(tag2);
+        assertEquals(expected, actual);
 
     }
 
     @Test
     void testRemoveTag() {
+        Tag tag = new Tag();
+        tag.setHashTag("test");
+        Tag tag2 = new Tag();
+        tag2.setHashTag("test2");
+        tagDao.addTag(tag);
+        tagDao.addTag(tag2);
+        tagDao.removeTag(tag2.getTagID());
+        List<Tag> actual = tagDao.getAllTags();
+
+        List<Tag> expected = new ArrayList<>();
+        expected.add(tag);
+        assertEquals(expected, actual);
 
     }
 
     @Test
     void testUpdateTag() {
-
+        Tag tag = new Tag();
+        tag.setHashTag("test");
+        Tag tag2 = new Tag();
+        tag2.setHashTag("test2");
+        tagDao.addTag(tag);
+        tagDao.addTag(tag2);
+        tag2.setHashTag("test2Update");
+        tagDao.updateTag(tag2);
+        Tag actual = tagDao.getTag(tag2.getTagID());
+        assertEquals(tag2, actual);
     }
 
-    @Test
-    void testGetByIDTag() {
-
-    }
 
 }
